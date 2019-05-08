570f4543cd0987e99be0650934cabf02deb78651
Mary Kate <MaryKate@marys-mbp-3.lan1>
2019-05-08 08:59:24 -0400

    add to componentdid mount

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React from 'react';
 import './App.css';
 
 const APIURL = `https://api.openbrewerydb.org/breweries`;
+const citySearch = //get user input from form
 
 class App extends Component {
   constructor() {
     super()
 
     this.state = {
-
-    }
-
+      error: null,
+      isLoaded: false,
+      itms: []
+    };
   }
 
   componentDidMount() {
+    fetch(APIURL + citySearch)
 
   }
 
