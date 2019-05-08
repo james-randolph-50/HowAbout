795fbf4f2abc252f83e1489dfef5a7b68006e733
Mary Kate <MaryKate@marys-mbp-3.lan1>
2019-05-08 09:15:56 -0400

    search and render error or breweries

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 const APIURL = `https://api.openbrewerydb.org/breweries`;
-const citySearch = //get user input from form
+//const citySearch = get user input from form
 
 class App extends Component {
   constructor() {
@@ -11,20 +11,46 @@ class App extends Component {
     this.state = {
       error: null,
       isLoaded: false,
-      itms: []
+      breweries: []
     };
   }
 
   componentDidMount() {
-    fetch(APIURL + citySearch)
-
+    fetch(APIURL)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            breweries: result.breweries
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
   }
 
   render() {
-
+    const { error, isLoaded, breweries } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
     return (
-      <div></div>
-    )
+          <ul>
+            {breweries.map(brewery => (
+              <li key={brewery.id}>
+              {brewery.name} {brewery.city}
+              </li>
+            ))}
+          </ul>
+     );
+    }
   }
 }
 
